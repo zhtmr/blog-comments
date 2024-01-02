e40a2e73e129dcabaeffd995d23ced4bf145e599
csw <susu199037@gmail.com>
2024-01-02 18:54:37 +0900

    .

--- .stylelintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['stylelint-config-standard'],
-  rules: {
-    'selector-class-pattern': '[A-z]+(-[a-z]+)*',
-    'color-function-notation': ['modern', { ignore: ['with-var-inside'] }],
-  },
-};
